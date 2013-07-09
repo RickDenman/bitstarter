b57a2396385a00dffcd6b1210f66039af8a2ebd4
Rick Denman <denman@southwestern.edu>
2013-07-09 09:06:00 +0000

    use index.html, read from file without syntax error

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var infile = fs.readfileSync("index.html");
+var infile = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
   response.send(infile.toString());
